de75bb0e19abebb76f81be55bba4f90daec1bbd9
TomYOON <31751481+TomYOON@users.noreply.github.com>
2021-12-07 23:51:36 +0900

    Create MemberServiceTest

---
+++ jpashop/src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,52 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) //좀 더 최적화됨
+@RequiredArgsConstructor //final로 선언된 변수에 자동으로 생성자를 만들어줌
+public class MemberService {
+
+
+    private final MemberRepository memberRepository; //final을 넣어주는게 좋음. 생성 체크가 가능하기때문
+
+//    @Autowired
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+    /**
+     * 회원 가입
+     */
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //exception
+        List<Member> findMembers = memberRepository.findByName(member.getName()); //이렇게 해도 동시접근할 경우 같은 이름 2개가 생성될 수 있음. 따라서 DB에 name 컬럼을 유니크로 설정해줘야함
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
