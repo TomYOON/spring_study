cd38fb5c7def60c7a4476fff247335cade91b5fd
TomYOON <31751481+TomYOON@users.noreply.github.com>
2022-04-07 21:25:32 +0900

    add x to many 최적화
    default_batch_fetch_size를 통한 최적화

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -97,11 +97,32 @@ public class OrderRepository {
         return query.getResultList();
     }
 
-    public List<Order> finAllWithMemberDelivery() {
+    public List<Order> findAllWithMemberDelivery() {
         return em.createQuery(
                 "select o from Order o" +
                         " join fetch o.member m" +
                         " join fetch o.delivery d", Order.class
         ).getResultList();
+    }
+
+    public List<Order> findAllWithItem() { //조인 때문에 order가 뻥튀기됨 (2개씩 넘어감)
+        return em.createQuery(
+                "select distinct o from Order o" +  //DB의 distinct와 다른 점은 order가 같은 id 값이면 중복을 제거해줌, DB는 row가 모두 같아야 제거
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class)
+                .getResultList();
+
+    }
+
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
     }
 }
