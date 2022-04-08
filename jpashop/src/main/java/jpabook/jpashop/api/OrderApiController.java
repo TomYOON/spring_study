003ab5a9e0654c6bbbb8e338f3c229523422bd4e
TomYOON <31751481+TomYOON@users.noreply.github.com>
2022-04-08 20:58:46 +0900

    add direct DTO query at once
    한 번에 가져오는 방식 (추천x)

--- jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ jpashop/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.api;
 
 import jpabook.jpashop.domain.*;
 import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.order.query.OrderFlatDto;
 import jpabook.jpashop.repository.order.query.OrderQueryDto;
 import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Getter;
@@ -76,9 +77,13 @@ public class OrderApiController {
 
     @GetMapping("/api/v5/orders")
     public List<OrderQueryDto> ordersV5() {
-        return orderQueryRepository.findOrderQueryDtos_optimization();
+        return orderQueryRepository.findOrderQueryDto_optimization();
     }
 
+    @GetMapping("/api/v6/orders")
+    public List<OrderFlatDto> ordersV6() {
+        return orderQueryRepository.findOrderQueryDto_flat();
+    }
     @Getter
     static class OrderDto {
 
